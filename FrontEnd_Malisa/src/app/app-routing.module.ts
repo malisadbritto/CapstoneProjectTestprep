93028753180f5a1e31f15f8447e6b0b0307b3d5f
Malisa <Mdbritto2567@conestogac.on.ca>
2020-03-18 11:22:02 -0400

    front end uploaded

--- FrontEnd_Malisa/src/app/app-routing.module.ts
+++ FrontEnd_Malisa/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-
+import{HomeComponent} from './home/home.component';
+import {TeamComponent} from './team/team.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
+  {path : '', component : HomeComponent},
   { path: 'login', component: LoginComponent },
-  {path : '', component : LoginComponent}
+  { path: 'register', component: RegisterComponent },
+  {path : 'home', component :HomeComponent},
+  {path : 'team', component : TeamComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'about', component : AboutComponent},
 
 ];
 
--- FrontEnd_Malisa/src/app/app.module.ts
+++ FrontEnd_Malisa/src/app/app.module.ts
@@ -9,12 +9,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    TeamComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
