c470c04eaae96d7ceb8ebd4d1f97c1ba11082d50
Malisa <Mdbritto2567@conestogac.on.ca>
2020-04-22 09:29:14 -0400

    delete all files

--- backend/models/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://fenil23lad:fenil23lad@cluster0-j2alj.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-
-require('./employee.model');
-require('./course.model');
